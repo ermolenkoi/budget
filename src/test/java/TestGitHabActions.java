5c33641a0272a9a5791ed9b1e56c9f9298369237
iermolenko <ermolenkoi@inbox.ru>
2021-11-15 18:43:01 +0400

    Test push with test fail

---
+++ src/test/java/TestGitHabActions.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class TestGitHabActions {
+
+    @Test
+    public void testSuccess() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testFail() {
+        assertTrue(false);
+    }
+}
